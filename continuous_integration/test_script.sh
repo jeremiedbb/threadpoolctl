bfddffb7fdfbb19e5a06dfdad77870fd767bea9b
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2019-09-16 20:34:29 +0200

    tst

--- continuous_integration/test_script.sh
+++ continuous_integration/test_script.sh
@@ -11,13 +11,14 @@ elif [[ "$PACKAGER" == "ubuntu" ]]; then
     source $VIRTUALENV/bin/activate
     PYTHONPATH="." python continuous_integration/display_versions.py
 
-    # ldd /usr/lib/python3.5/dist-packages/numpy/linalg/lapack_lite*
-    sudo find / -name 'lapack_lite*'
+    ldd /usr/lib/python3/dist-packages/numpy/linalg/lapack_lite*
+    # sudo find / -name 'lapack_lite*'
 
     if [[ ! -z $APT_BLAS2 ]]; then
         sudo apt-get install $APT_BLAS2
     fi
 
+    ldd /usr/lib/python3/dist-packages/numpy/linalg/lapack_lite*
     PYTHONPATH="." python continuous_integration/display_versions.py
 
     # ldd /usr/lib/python3.5/dist-packages/numpy/linalg/lapack_lite*
